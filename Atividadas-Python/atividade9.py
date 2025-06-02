72d969eba974d5330c030b6c45685859e06b7bac
Guilherme Duarte <guizinh4xx@gmail.com>
2025-06-02 20:12:46 -0300

    adicionei a atividade 9

---
+++ Atividadas-Python/atividade9.py
@@ -0,0 +1,26 @@
+#Crie um programa que leia um número e faça sua tabuada completa!
+
+num = int(input("Digite um número para ver sua tabuada: "))
+x1 = num * 1
+x2 = num * 2
+x3 = num * 3
+x4 = num * 4
+x5 = num * 5
+x6 = num * 6
+x7 = num * 7
+x8 = num * 8
+x9 = num * 9
+x10 = num * 10
+print(f"Tabuada do {num}:")
+print('=============')
+print(f"{num} X 1  = {x1}")
+print(f"{num} X 2  = {x2}")
+print(f"{num} X 3  = {x3}")
+print(f"{num} X 4  = {x4}")
+print(f"{num} X 5  = {x5}")
+print(f"{num} X 6  = {x6}")
+print(f"{num} X 7  = {x7}")
+print(f"{num} X 8  = {x8}")
+print(f"{num} X 9  = {x9}")
+print(f"{num} X 10 = {x10}")
+print('=============')
