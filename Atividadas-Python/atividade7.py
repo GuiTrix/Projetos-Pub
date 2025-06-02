7e55b8474dc3f0e8b3fc63efb56530d8b23fa3e4
Guilherme Duarte <guizinh4xx@gmail.com>
2025-06-02 19:51:20 -0300

    Adicionei a atividade 7

---
+++ Atividadas-Python/atividade7.py
@@ -0,0 +1,9 @@
+bi1 = float(input("Digite sua nota do primeiro bimestre: "))
+bi2 = float(input("Digite sua nota do segundo bimestre: "))
+bi3 = float(input("Digite sua nota do terceiro bimestre: "))
+bi4 = float(input("Digite sua nota do quarto bimstre: "))
+
+soma = bi1 + bi2 + bi3 + bi4
+media = soma / 4
+
+print(f"Sua média esse ano foi de {media}!") 
