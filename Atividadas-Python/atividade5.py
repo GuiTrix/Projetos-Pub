ea9bd132d774e56628402cbee216b8ec4b942e67
Guilherme Duarte <guizinh4xx@gmail.com>
2025-06-02 19:27:53 -0300

    adicionei a atividade 5

---
+++ Atividadas-Python/atividade5.py
@@ -0,0 +1,7 @@
+#Crie um programa que leia um número inteiro e mostre seu antecessor e sucessor!
+
+num = int(input("Digite um número: "))
+antecessor = num - 1
+sucessor = num + 1
+
+print(f"O sucessor do número {num} é {sucessor} e seu antecessor é {antecessor}!")
