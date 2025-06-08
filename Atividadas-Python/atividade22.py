99c944edfe83f123f2722b9843dbd7d132440cea
Guilherme Duarte <guizinh4xx@gmail.com>
2025-06-08 11:06:20 -0300

    fiz a a atividade 22

---
+++ Atividadas-Python/atividade22.py
@@ -0,0 +1,5 @@
+# Faça um programa q deixa seu nome em maiusculas e minusculas
+
+nome = str(input("Digite seu nome ou nome de algo: ")).strip()
+print("Seu nome em Maisculas é {}".format(nome.upper()))
+print("Seu nome em Minusculas é {}".format(nome.lower()))
